04892574c944ee68d269a675f81dbb74e3f26e3c
Diste2022 <111429016+Diste2022@users.noreply.github.com>
2022-10-09 16:33:22 +0800

    Add files via upload

---
+++ platinum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(viod)
+{
+  float weight;
+  float value;
+  printf("plz enter your weight");
+  scanf("%f",&weight);
+  value=1700.0*weight*14.5833;
+  
+  printf("your weight is $%.2f.\n",value);
+  
+    return 0;
+ }
